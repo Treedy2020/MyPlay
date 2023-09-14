3f8b3b296b52c5e01fafa2c6ae859936a72573a6
RichardCui <8263408+saber3421@user.noreply.gitee.com>
2023-09-14 13:51:39 +0800

    show in png files.

--- main.py
+++ main.py
@@ -1,45 +1,39 @@
 import json
-import fitz
 import streamlit as st
-from pdf2image import convert_from_path
 from PIL import Image
-
-@st.cache_data
-def get_pdf_image(path):
-    return convert_from_path(path)
 
 if 'page_ind' not in st.session_state:
     st.session_state.page_ind = 1
     
-if 'pdf_1' not in st.session_state:
-    st.session_state.pdf_1 = get_pdf_image('./show_data/ECE-100_03.pdf')
-if 'pdf_2' not in st.session_state:
-    st.session_state.pdf_2 = get_pdf_image('./show_data/ECE-100_02 Amend 4.pdf')
-    
+def onclik_add():
+    st.session_state.page_ind += 1
 
+def onclik_sub():
+    st.session_state.page_ind -= 1
+    
 pre, current, next = st.columns(3)
 with pre:
-    if st.button('Previous', disabled=st.session_state.page_ind == 0):
-        st.session_state.page_ind -= 1
-
+    st.button('Previous', disabled=st.session_state.page_ind == 1, on_click=onclik_sub)
+    
 with current:
     st.button(f'Current: {st.session_state.page_ind}', disabled=True)
+    
 with next:
-    if st.button('Next', disabled=st.session_state.page_ind == 105):
-        st.session_state.page_ind += 1
+    st.button('Next', disabled=st.session_state.page_ind == 106, on_click=onclik_add)
 
 with open('./show_data/res_show_in_ui.json', 'r') as f:
     st.session_state.show_data = json.load(f)
 
-ind_1, ind_2, diff = st.session_state.show_data[str(int(st.session_state.page_ind) - 1)]
+ind_1, ind_2, diff = st.session_state.show_data[str(st.session_state.page_ind - 1)]
+image_path_1, image_path_2 = f'./show_data/ECE-100_03/page_{ind_1 + 1}.png', f'./show_data/ECE-100_02/page_{ind_2 + 1}.png'
 
 col1, col2 = st.columns(2)
 with col1:
     tab1, tab2 = st.tabs(['ECE-100_03.pdf', 'ECE-100_02 Amend 4.pdf'])
     with tab1:
-        st.image(st.session_state.pdf_1[ind_1], caption=f'ECE-100_03.pdf Page {ind_1 + 1}')
+        st.image(image_path_1, caption=f'ECE-100_03.pdf Page {ind_1 + 1}')
     with tab2:
-        st.image(st.session_state.pdf_2[ind_2], caption=f'ECE-100_02 Amend 4.pdf Page {ind_2 + 1}')
+        st.image(image_path_2, caption=f'ECE-100_02 Amend 4.pdf Page {ind_2 + 1}')
 with col2:
     st.info(diff)
 
